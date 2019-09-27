1f42ef66e63bdc49c4d72fca93ec103eeda38ef3
JWangNov <55171746+JWangNov@users.noreply.github.com>
2019-09-27 19:06:45 -0400

    Update try_lite.py

--- PART - Google Natural Language API/Readme.md
+++ PART - Google Natural Language API/Readme.md
@@ -2,13 +2,13 @@
 
 
 
-## To Use It on Mac OS
+## To Use It on Mac OS (***hard to work...***)
 
 ### Set 'Path'
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/wangjialun/Desktop/EC601/Mini\ Project/EC601\ mini\ project-34898095f322.json"
 
 ### Activate the VirtualEnv
-cd /Users/wangjialun/Desktop/EC601/Mini\ Project/PART\ -\ Google\ Natural\ Language\ API
+cd /Users/wangjialun/Desktop/Mini\ Project/Google_NL_API
 
 virtualenv --python python3 env
 
@@ -22,16 +22,30 @@ deactivate
 
 
 
-## To Use It on Linux ***Devbox***
+## To Use It on Linux ***Devbox, the prefered way to make it!***
 
 ### Set 'Path' on Linux
 export GOOGLE_APPLICATION_CREDENTIALS="/media/sf_Mini_Project/EC601 mini project-34898095f322.json"
+
+Of course you can change the 'PATH' to your one, by the following line:
+
+`export GOOGLE_APPLICATION_CREDENTIALS="PATH"`
 
 
 
 ## Some File Descriptions
 
 ### ***analyze_sentiment.py***
-It works! But till now, it can only recognize from an input string. Try by the following line:
+It works! 
+But till now, it can only recognize from an input string. 
+Try by the following line:
 
 python analyze_sentiment.py
+
+
+### ***try_lite.py***
+It works! 
+It can return directly the sentiment score of a paragraph. 
+Try by the following line:
+
+python try_lite.py
--- PART - Google Natural Language API/try_lite.py
+++ PART - Google Natural Language API/try_lite.py
@@ -1,20 +1,24 @@
+'''
+python try_lite.py
+'''
+
 from google.cloud import language_v1
 from google.cloud.language_v1 import enums
 import six
 
-def sample_analyze_sentiment(content):
+def analyze_sentiment(content):
 
-    client = language_v1.LanguageServiceClient()
+	client = language_v1.LanguageServiceClient()
+	if isinstance(content, six.binary_type):
+		content = content.decode('utf-8')
+	type_ = enums.Document.Type.PLAIN_TEXT
+	document = {'type': type_, 'content': content}
+	response = client.analyze_sentiment(document)
+	sentiment = response.document_sentiment
+	
+	return sentiment.score , sentiment.magnitude
 
-    # content = 'Your text to analyze, e.g. Hello, world!'
 
-    if isinstance(content, six.binary_type):
-        content = content.decode('utf-8')
-
-    type_ = enums.Document.Type.PLAIN_TEXT
-    document = {'type': type_, 'content': content}
-
-    response = client.analyze_sentiment(document)
-    sentiment = response.document_sentiment
-    print('Score: {}'.format(sentiment.score))
-    print('Magnitude: {}'.format(sentiment.magnitude))
+if __name__ == '__main__':
+	aaa = '0  On National Voter Registration Day, its up to us as citizens to make sure everyone we know can make their voices h https:t.corYRdykNARc'
+	print('The content to analyze is:\n{}\nThe sentiment score of it is: [{}]' . format(aaa, analyze_sentiment(aaa)[0]))
